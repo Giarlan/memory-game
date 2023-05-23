5bc890014aa618b8ae8b179dcf4b3cdfdf61ec50
Giarlan Santana <giarlansilva@yahoo.com>
2023-05-23 20:18:10 -0300

    SET BOARD USING JAVASCRIPT
       *Build - It has been developed. The js code to set the board

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
-const FRONT = "card-front";
-const BACK = "card-back";
+const FRONT = 'card-front';
+const BACK = 'card-back';
+const CARD = 'card';
+const ICON = 'icon';
 
 let techs = [
     'bootstrap',
@@ -21,6 +23,52 @@ startGame();
 function startGame() {
     cards = createCardsFromTechs(techs);
     shuffleCards(cards);
+    console.log(cards);
+
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById('gameBoard');
+
+    cards.forEach( card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+
+}
+
+function createCardContent(card, cardElement) {
+
+    createCardFace(FRONT, card, cardElement);    
+    createCardFace(BACK, card, cardElement);  
+}
+
+function createCardFace(face, card, element){
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if(face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = '../assets/images/' + card.icon + '.png';
+        cardElementFace.appendChild(iconElement);
+    } else {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = '../assets/images/restart.png';
+        cardElementFace.appendChild(iconElement);
+    }
+    element.appendChild(cardElementFace);
+
 }
 
 function shuffleCards(cards) {
@@ -40,9 +88,9 @@ createCardsFromTechs(techs);
 function createCardsFromTechs(techs) {
     let cards = [];
 
-    for(let tech of techs) {
+    techs.forEach( (tech) => {
         cards.push(createPairFromTech(tech));
-    }
+    })
 
     return cards.flatMap(pair => pair);
 }
@@ -61,5 +109,11 @@ function createPairFromTech(tech) {
 }
 
 function createIdWithTech(tech) {
-    return tech + parseInt(Math.ramdom() *1000);
+    return tech + parseInt(Math.random() *1000);
+}
+
+function flipCard() {
+
+    this.classList.add('flip');
+
 }
