c57fa1bb0000aad0ccabce2f06417a4b43c69a48
Giarlan Santana <giarlansilva@yahoo.com>
2023-05-23 18:34:03 -0300

    SHUFFLE THE CARDS
       *Build - It has created a function for the feature of shuffling the cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,27 @@ let techs = [
     'vuejs'
 ];
 
+let cards = null;
+
+startGame();
+
+function startGame() {
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length;
+    let ramdomIndex = 0;
+
+    while(currentIndex !== 0) {
+        ramdomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex --;
+
+        [cards[ramdomIndex], cards[currentIndex]] = [cards[currentIndex], cards[ramdomIndex]];
+    }
+}
+
 createCardsFromTechs(techs);
 
 function createCardsFromTechs(techs) {
